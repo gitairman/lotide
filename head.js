ea5922b55d90f10caf60679cd5793ba3413527b6
Aaron Hopkins <aaronhopkins86@gmail.com>
2024-03-04 13:23:31 -0800

    implemented head function

---
+++ head.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  const result = actual === expected;
+  console.log(
+    `${result ? '✅✅✅' : '🛑🛑🛑'} Assertion ${
+      result ? 'Passed' : 'Failed'
+    }: ${actual} ${result ? '===' : '!=='} ${expected}`
+  );
+};
+
+const head = (arr) => {
+  return arr[0];
+};
+
+// TEST CODE
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+console.log(`value is: ${head([])}`);
