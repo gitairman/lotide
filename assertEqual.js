9158bc7800286ab821c3bee17674d690027a2fa9
Aaron Hopkins <aaronhopkins86@gmail.com>
2024-03-04 13:03:31 -0800

    updated output

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,14 @@
-const assertEqual = function(actual, expected) {
-  const result = actual === expected;
+const assertEqual = function (actual, expected) {
+  const result = actual === expected
   console.log(
     `${result ? '✅✅✅' : '🛑🛑🛑'} Assertion ${
       result ? 'Passed' : 'Failed'
-    }: ${actual} is ${result ? 'EQUAL' : 'NOT EQUAL'} to ${expected}`
-  );
-};
+    }: ${actual} ${result ? '===' : '!=='} ${expected}`
+  )
+}
 
 // TEST CODE
-assertEqual('Lighthouse Labs', 'Bootcamp');
-assertEqual(1, 1);
-assertEqual(1, 5);
-assertEqual('Lighthouse Labs', 'Lighthouse Labs');
+assertEqual('Lighthouse Labs', 'Bootcamp')
+assertEqual(1, 1)
+assertEqual(1, 5)
+assertEqual('Lighthouse Labs', 'Lighthouse Labs')
