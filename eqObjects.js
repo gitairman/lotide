087e75d15f8194fcaec470f07bd3ecc794f8ed7f
Aaron Hopkins <aaronhopkins86@gmail.com>
2024-03-06 12:46:03 -0800

    fixed bug where checking only if obj2 key is undefined

--- eqObjects.js
+++ eqObjects.js
@@ -8,9 +8,7 @@ const assertEqual = function (actual, expected) {
 }
 
 const eqArrays = (arr1, arr2) =>
-  arr1.length === arr2.length && Array.isArray(arr1) && Array.isArray(arr2)
-    ? arr1.every((e, i) => e === arr2[i])
-    : false
+  arr1.length === arr2.length ? arr1.every((e, i) => e === arr2[i]) : false
 
 // eqObjects takes in two objects and returns true or false, based on a perfect match.
 const eqObjects = (obj1, obj2) => {
@@ -18,7 +16,8 @@ const eqObjects = (obj1, obj2) => {
   for (let [key, val] of Object.entries(obj1)) {
     if (
       // if a key in obj1 is not found in obj2 or the value in matching keys is not the same return false
-      !obj2[key] || Array.isArray(val)
+      !obj2.hasOwnProperty(key) ||
+      (Array.isArray(val) && Array.isArray(obj2[key]))
         ? !eqArrays(val, obj2[key])
         : obj2[key] !== val
     )
