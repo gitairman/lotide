f094d8fac2160ce5b36bd6c8d39b25c435111542
Aaron Hopkins <aaronhopkins86@gmail.com>
2024-03-06 07:10:58 -0800

    implemented middle.js

---
+++ middle.js
@@ -0,0 +1,27 @@
+const eqArrays = (arr1, arr2) =>
+  arr1.length === arr2.length && Array.isArray(arr1)
+    ? arr1.every((e, i) => e === arr2[i])
+    : false
+
+const assertArraysEqual = function (arr1, arr2) {
+  const result = eqArrays(arr1, arr2)
+  console.log(
+    `${result ? '✅✅✅' : '🛑🛑🛑'} Assertion ${
+      result ? 'Passed' : 'Failed'
+    }: ${arr1} ${result ? '===' : '!=='} ${arr2}`
+  )
+}
+// return the middle elements of an array
+const middle = (arr) => {
+  // if array has 2 elements or less, there is no middle so return empty array
+  if (arr.length <= 2) return []
+  const even = arr.length % 2 === 0,
+    midStartIdx = even ? arr.length / 2 - 1 : Math.floor(arr.length / 2),
+    midEndIdx = even ? midStartIdx + 2 : midStartIdx + 1
+  return arr.slice(midStartIdx, midEndIdx)
+}
+
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+assertArraysEqual(middle([1]), [])
+assertArraysEqual(middle([1, 2]), [])
