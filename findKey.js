42dd8900261dca1e5700643f3a149adc96e434f6
Aaron Hopkins <aaronhopkins86@gmail.com>
2024-03-07 14:48:38 -0800

    implemented findKey

---
+++ findKey.js
@@ -0,0 +1,85 @@
+const assertEqual = function (actual, expected) {
+  const result = actual === expected
+  console.log(
+    `${result ? '✅✅✅' : '🛑🛑🛑'} Assertion ${
+      result ? 'Passed' : 'Failed'
+    }: ${actual} ${result ? '===' : '!=='} ${expected}`
+  )
+}
+
+const findKey = (obj, cb) => {
+  for (const [key, val] of Object.entries(obj)) {
+    if (cb(val)) return key
+  }
+}
+
+findKey(
+  {
+    'Blue Hill': { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 2
+)
+// => "noma"
+
+// TEST CODE
+assertEqual(
+  findKey(
+    {
+      'Blue Hill': { stars: 1 },
+      Akaleri: { stars: 3 },
+      noma: { stars: 2 },
+      elBulli: { stars: 3 },
+      Ora: { stars: 2 },
+      Akelarre: { stars: 3 },
+    },
+    (x) => x.stars === 2
+  ),
+  'noma'
+)
+assertEqual(
+  findKey(
+    {
+      'Blue Hill': { stars: 1 },
+      Akaleri: { stars: 3 },
+      noma: { stars: 2 },
+      elBulli: { stars: 3 },
+      Ora: { stars: 2 },
+      Akelarre: { stars: 3 },
+    },
+    (x) => x.stars === 3
+  ),
+  'Akaleri'
+)
+assertEqual(
+  findKey(
+    {
+      'Blue Hill': { stars: 1 },
+      Akaleri: { stars: 3 },
+      noma: { stars: 2 },
+      elBulli: { stars: 3 },
+      Ora: { stars: 2 },
+      Akelarre: { stars: 3 },
+    },
+    (x) => x.stars === 1
+  ),
+  'Blue Hill'
+)
+assertEqual(
+  findKey(
+    {
+      'Blue Hill': { stars: 1 },
+      Akaleri: { stars: 3 },
+      noma: { stars: 2 },
+      elBulli: { stars: 3 },
+      Ora: { stars: 2 },
+      Akelarre: { stars: 3 },
+    },
+    (x) => x.stars === 4
+  ),
+  undefined
+)
